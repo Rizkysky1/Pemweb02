85e703ab9953574b34a4c382f396e93b6b160d23
Rizkysky1 <114985535+Rizkysky1@users.noreply.github.com>
2023-03-06 08:29:31 +0700

    Add files via upload

---
+++ P2_RizkyRamadhan_TI13/index2.php
@@ -0,0 +1,10 @@
+<?php
+    $Costomer= $_POST ['Costomer'];
+    $Produk= $_POST ['Produk'];
+    $Jumlah= $_POST ['Jumlah'];
+
+    echo 'Customer : ' .$Costomer;
+    echo '<br> Produk yang di pilih : ' . $Produk;
+    echo '<br> Jumlah Item : ' . $Jumlah;
+
+?>
